6dc3918aa576644629ed734a6c739d6b381354ec
s-chijo <s-chijo@53S500042468.it.bsd405.org>
2019-11-14 11:48:49 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,58 @@
 
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		
+		while(true)	{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Add, subtract, multiply, or divide fractions: ");
+			String input = userInput.nextLine();
+			
+			produceAnswer(input);   
+			System.out.println("If you want to quit now, just type in \"quit\" ");
+			String exit = userInput.nextLine();     //Takes user input 
+			
+			if(exit.equals("quit")) { //Method for users to quit the program
+				System.exit(0);     
+	
+		}
+		}
 
-    }
+	}
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+	private static void While(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 
-        return "";
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		// TODO: Implement this function to produce the solution to the input
+		int space = input.indexOf(" ");    
+    	String first = input.substring(0 , space);    //The first operand from the user input
+    	String operator = input.substring(space + 1, space + 2);    //The operator between the operands
+    	String second = input.substring(space + 3);   //The second operand from the user input
+   
+    	
+        return second; //returns the second operand
+		
+	}
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
 
 }
