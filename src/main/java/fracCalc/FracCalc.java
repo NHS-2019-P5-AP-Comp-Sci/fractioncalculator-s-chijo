ac627f3cbb174ed3d076eabd755352a841d0379f
s-chijo <s-chijo@53S500042468.it.bsd405.org>
2019-12-09 22:35:25 -0800

    Final

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -23,98 +23,143 @@ public class FracCalc {
 
 	}
 
-	private static void While(boolean b) {
-		// TODO Auto-generated method stub
+	public static String produceAnswer(String equation) {
+// Parses operator and operand
+		int space = equation.indexOf(' ');
+		String operand1 = equation.substring(0, space);
+		String operator = equation.substring(equation.indexOf(" ") + 1, space + 2);
+		String operand2 = equation.substring(space + 3);
+
+// Parses numerator, denominator
+		String one = operandSeg(operand1);
+		int num1 = Integer.parseInt(one.substring(0, one.indexOf('/')));
+		int den1 = Integer.parseInt(one.substring(one.indexOf('/') + 1));
+		String two = operandSeg(operand2);
+		int num2 = Integer.parseInt(two.substring(0, two.indexOf('/')));
+		int den2 = Integer.parseInt(two.substring(two.indexOf('/') + 1));
+
+// For adding
+		if (operator.equals("+")) {
+			String num = (num1 * den2) + (num2 * den1) + "";
+			String den = den1 * den2 + "";
+			String answer = num + "/" + den;
+			return simplify(answer);
+
+// For subtracting
+		} else if (operator.equals("-")) {
+			String num = (num1 * den2) - (num2 * den1) + "";
+			String den = den1 * den2 + "";
+			String answer = num + "/" + den;
+			return simplify(answer);
+
+// For dividing
+		} else if (operator.equals("/")) {
+			if (num1 == 0 || den1 == 0 || num2 == 0 || den2 == 0)
+				return "0";
+			String num = num1 * den2 + "";
+			String den = den1 * num2 + "";
+			String answer = num + "/" + den;
+			return simplify(answer);
+
+// For multiplying
+		} else if (operator.equals("*")) {
+			if (num1 == 0 || den1 == 0 || num2 == 0 || den2 == 0)
+				return "0";
+			String num = num1 * num2 + "";
+			String den = den1 * den2 + "";
+			String answer = num + "/" + den;
+			return simplify(answer);
+
+		} else {
+			return "ERROR: invalid format.";
+		}
 
 	}
 
-	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
-	// test your code
-	// This function takes a String 'values' and produces the result
-	//
-	// values is a fraction string that needs to be evaluated. For your program,
-	// this
-	// will be the user values.
-	// e.g. values ==> "1/2 + 3/4"
-	//
-	// The function should return the result of the fraction after it has been
-	// calculated
-	// e.g. return ==> "1_1/4"
+	public static String operandSeg(String operand) {
+// Sets equation as a string
+		String whole;
+		String num;
+		String den;
 
-	public static String produceAnswer(String values) {
-		int space = values.indexOf(" ");
-		String equation1 = values.substring(0, space);
-		String symbol = values.substring(space + 1, space + 2);
-		String equation2 = values.substring(space + 3);
-		String two = negative(equation2);
-		String one = negative(equation1);
-		int numerator1 = Integer.parseInt(one.substring(0, one.indexOf('/')));
-		int denominator1 = Integer.parseInt(one.substring(one.indexOf('/') + 1));
-		int numerator2 = Integer.parseInt(two.substring(0, two.indexOf('/')));
-		int denominator2 = Integer.parseInt(two.substring(two.indexOf('/') + 1));
-		if (symbol.equals("-")) {
-			String numerator = (numerator1 * denominator2) - (numerator2 * denominator1) + "";
-
-			String denominator = denominator1 * denominator2 + "";
-			String result = numerator + "/" + denominator;
-
-			return result;
-
-		} else if (symbol.equals("+")) {
-			String numerator = (numerator1 * denominator2) + (numerator2 * denominator1) + "";
-
-			String denominator = denominator1 * denominator2 + "";
-			String result = numerator + "/" + denominator;
-
-			return result;
-
-		} else if (symbol.equals("*")) {
-			String numerator = numerator1 * numerator2 + "";
-			String denominator = denominator1 * denominator2 + "";
-			String result = numerator + "/" + denominator;
-
-			return result;
-
-		} else {
-			String numerator = numerator1 * denominator2 + "";
-			String denominator = denominator1 * numerator2 + "";
-			String result = numerator + "/" + denominator;
-
-			return result;
-
-		} 
-
+// To whole number if it is mixed number
+		if (operand.indexOf('_') != -1) {
+			whole = operand.substring(0, operand.indexOf('_'));
 		}
-	
-	public static String negative(String equation) {
-		String wholeNum;
-		String numerator;
-		String denominator;
-		if (equation.indexOf('_') != -1) {
-			wholeNum = equation.substring(0, equation.indexOf('_'));
-		} else if (equation.indexOf('/') != -1) {
-			wholeNum = "0";
-		} else {
-			wholeNum = equation.substring(0, equation.length());
+// Sets whole to 0 if number is fraction
+		else if (operand.indexOf('/') != -1) {
+			whole = "0";
 		}
-		if (equation.indexOf('/') != -1) {
-			numerator = equation.substring(equation.indexOf('_') + 1, equation.indexOf('/'));
-			denominator = equation.substring(equation.indexOf('/') + 1);
-		}
+
 		else {
-			numerator = "0";
-			denominator = "1";
+			whole = operand.substring(0, operand.length());
 		}
-		if (wholeNum.length() > 0) {
-			if (wholeNum.substring(0, 1).equals("-")) {
-				numerator = "-" + (Integer.parseInt(denominator) * Integer.parseInt(wholeNum.substring(1))
-						+ Integer.parseInt(numerator));
-			} else if (!wholeNum.substring(0, 1).equals("-")) {
-				
-				numerator = Integer.parseInt(denominator) * Integer.parseInt(wholeNum) + Integer.parseInt(numerator)
-						+ "";
+
+// Sets num and den if operand is a fraction
+		if (operand.indexOf('/') != -1) {
+			num = operand.substring(operand.indexOf('_') + 1, operand.indexOf('/'));
+			den = operand.substring(operand.indexOf('/') + 1);
+		}
+// Sets numerator and denominator if answer is a whole
+		else {
+			num = "0";
+			den = "1";
+		}
+
+//Converts to mixed number
+		if (whole.length() > 0) {
+			if (whole.substring(0, 1).equals("-")) {
+				num = "-" + (Integer.parseInt(den) * Integer.parseInt(whole.substring(1)) + Integer.parseInt(num));
+			} else if (!whole.substring(0, 1).equals("-")) {
+				num = Integer.parseInt(den) * Integer.parseInt(whole) + Integer.parseInt(num) + "";
 			}
 		}
-		return numerator + "/" + denominator;
+		return num + "/" + den;
+	}
+
+	public static String simplify(String equation) {
+// Parses into numerator, denominator, and whole
+		int num = Integer.parseInt(equation.substring(0, equation.indexOf("/")));
+		int den = Integer.parseInt(equation.substring(equation.indexOf("/") + 1));
+		int whole = num / den;
+
+// Negative placeholder
+		if ((num < 0 && den < 0) || (num > 0 && den < 0)) {
+			num /= -1;
+			den /= -1;
+		}
+
+// simplifies outputs equal to 0
+		if (num == 0 || den == 0)
+			return "0";
+
+// finds and divides by common den
+		for (int i = Math.abs(den); i > 0; i--) {
+			if ((num % i == 0) && (den % i == 0)) {
+				num = num / i;
+				den = den / i;
+			}
+		}
+
+		if (den == 1) {
+			return num + "";
+
+		} else if (num % den == 0 && den != 0) {
+			return (num / den) + "";
+		}
+
+		else {
+			num = num - (whole * den);
+			if (whole == 0) {
+				return num + "/" + den;
+			} else {
+				if (whole < 0 && den < 0)
+					den /= -1;
+				else if (whole < 0 && num < 0)
+					num /= -1;
+				return "" + whole + "_" + num + "/" + den;
+			}
+		}
+
 	}
 }
